f3fb54427a2ba165af42f4249e5b1e06bc666d5d
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2019-11-27 13:07:02 -0600

    Fixed time formatting

--- profileinfo.js
+++ profileinfo.js
@@ -1,4 +1,6 @@
 
+var availableTimes = [];
+
 function populateData () {
   var temp = localStorage.getItem("myName");
   var temp1 = localStorage.getItem("myEmail");
@@ -22,14 +24,39 @@ function populateData () {
   document.getElementById('displayGender').innerHTML = temp3;
   }
 }
+
 function submitTime(){
-  temp=document.getElementById("timeAdd")
-  var happy = document.createElement("li")
-  var textNode=document.createTextNode(temp.value)
-  happy.appendChild(textNode)
-  var list= document.getElementById("listofTimes")
-  list.appendChild(textNode)
-  temp.value=""
-  tempee=document.getElementById('defaulttext')
-  tempee.innerHTML="Your listed times"
+  var inputTime = document.getElementById("timeAdd").value;
+  console.log('Input time:', typeof(inputTime));
+  var h = parseInt(inputTime.slice(0,2));
+  var m = parseInt(inputTime.slice(3,5));
+  console.log('hours/minutes',h, m);
+
+  formattedTime = formatAMPM(h,m);
+  console.log("formatted time", formattedTime);
+
+  availableTimes.push(formattedTime);
+  document.getElementById("listofTimes").innerHTML = "";
+  for (var i = 0; i < availableTimes.length; i++)
+  {
+       nameList = "<li>" + availableTimes[i] + "</li>";
+       document.getElementById("listofTimes").innerHTML += nameList;
+  }
+  // temp = document.getElementById("timeAdd")
+  // var happy = document.createElement("li")
+  // var textNode = document.createTextNode(temp.value)
+  // happy.appendChild(textNode)
+  // var list = document.getElementById("listofTimes")
+  // list.appendChild(textNode)
+  // temp.value=""
+  document.getElementById('defaulttext').innerHTML="Your listed times"
+}
+
+function formatAMPM(hours, minutes) {
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
 }
