cc811e0feb8883bc62692b0ae264300d3469c4d6
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2019-11-27 15:57:10 -0600

    Moved filter component to connect to friends page

--- filters.js
+++ filters.js
@@ -32,11 +32,12 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("btnf");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    console.log('new classname', this.className);
   });
 }
